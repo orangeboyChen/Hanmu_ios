f99f751a1f6f49db4f334017f6bfa7e84aa1fe0d
orangeboy <chenenhan@qq.com>
2021-03-11 01:03:20 +0800

    Initial Commit

---
+++ Hanmu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hanmu
+//
+//  Created by orangeboy on 2021/3/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hanmu/HanmuApp.swift
@@ -0,0 +1,17 @@
+//
+//  HanmuApp.swift
+//  Hanmu
+//
+//  Created by orangeboy on 2021/3/11.
+//
+
+import SwiftUI
+
+@main
+struct HanmuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
